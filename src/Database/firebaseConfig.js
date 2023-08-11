e938fb3f14651416e94281c75f85bcd7f384c8cb
Samuel Herrera <samuelmhg97@gmail.com>
2023-08-10 23:01:19 -0300

    location

--- src/Database/firebaseConfig.js
+++ src/Database/firebaseConfig.js
@@ -1,3 +1,5 @@
 export const realtime_database_url = "https://reactnative-95d99-default-rtdb.firebaseio.com/"
 
 export const api_key = "AIzaSyAVPW_RISMrVEa2hHiSbdQzjngnav50D5g"
+
+export const google_maps_api_key = "AIzaSyAt0gf9jtHo4ZdQsdh5Tk7ub7zZCjE8dBY" 
